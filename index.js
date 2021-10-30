8027e9a45859e68d9604a968f70e6ab9c49141f4
Goran Belanovic <goranbelanovic@yahoo.com>
2021-10-30 15:29:08 +0200

    'msg'

--- index.js
+++ index.js
@@ -4,27 +4,81 @@ const app = express();
 const http = require('http');
 const homeRoute = require('./routes/home');
 const HOST_BACKEND = require('./hostBackend.js');
+const mongoose = require('mongoose');
+const Message = require('./models/Message');
+
+////////// mongodb initialise
+
+const mongoAddress1 = `mongodb://localhost/news`;
+const mongoAddress2 = `mongomongodb+srv://goranbelanovic:1234@cluster0.xneom.mongodb.net/chat?retryWrites=true&w=majority`;
+
+// mongoose.set('useFindAndModify', false);
+
+mongoose.connect(mongoAddress2, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to the chat database'))
+  .catch(err => console.log(err))
+
+///////////////////////////////
 
 app.use(homeRoute);
 const server = http.createServer(app);
 const io = socketIO(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-      allowedHeaders: ["my-custom-header"],
-      credentials: true
-    }});
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
-io.on('connection', (socket) => {
-    
-    socket.on('message', (payload) => {
-      io.emit('message', payload);
-    }); 
-    socket.on('check', (payload) => {
-      io.emit('check', payload);
-    });
+io.on('connection', async (socket) => {
+  /* 
+      socket.on('initial-rooms', (rooms) => {
+        rooms.forEach(prom => {
+          socket.join(prom);
+        });
+      }) */
+  const messagesDB = await Message.find();
+
+  const timeOfLastMessage = messagesDB[messagesDB.length - 1].milliseconds;
+  const deletedMessages = await Message.deleteMany({milliseconds: {$lt: timeOfLastMessage - 60000} })
+
+
+  socket.emit('messagesDB', messagesDB);
+
+  socket.on('requestMessagesDB', () => socket.emit('messagesDB', messagesDB));
+
+  socket.on('message', async (payload) => {
+
+    /*  console.log(socket.rooms)
+     let messageRoom;
+     for(const prom in socket.rooms) {
+       console.log(socket.rooms[prom])
+       if(socket.rooms[prom] === payload.room) {
+         messageRoom = socket.rooms[prom];
+       }
+     } */
+    /* socket.join(payload.room); */
+
+    const oneMessage = new Message(payload);
+
+    try {
+      const savedPayload = await oneMessage.save();
+      io.emit('message', savedPayload);
+    } catch (err) {
+        console.log(err);
+    }
+  });
+
+  socket.on('check', async (payload) => {
+    const checkedMessage = await Message.findByIdAndUpdate(payload._id, {checked: payload.checked}, {new: true})
+    console.log(checkedMessage);
+    io.emit('check', payload);
+  });
 
 })
+
+// app.get('/deleteMessages', (req, res) => {console.log('deleteMessages')});
 
 const port = process.env.PORT || 4001;
 server.listen(port, HOST_BACKEND, () => console.log(`Listening on port ${port}`));

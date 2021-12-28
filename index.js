6d9d0e6b6740e4b6221c5dbcf50233ab7cf4f2ba
belanovic <goranbelanovic@yahoo.com>
2021-12-28 15:10:29 +0100

    'msg'

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const homeRoute = require('./routes/home');
 const mongoose = require('mongoose');
 const Message = require('./models/Message');
 
-///////////////// mongodb initialise   
+///////////////// mongodb initialisee
 
 const mongoAddress1 = `mongodb://localhost/news`;
 const mongoAddress2 = `mongomongodb+srv://goranbelanovic:1234@cluster0.xneom.mongodb.net/chat?retryWrites=true&w=majority`;
